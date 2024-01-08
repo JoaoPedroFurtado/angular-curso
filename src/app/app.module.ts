fb412666418738ce1aea85f66f8d42bb5beda91b
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-01-08 10:19:41 -0300

    feat: criado componente change-number e emitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from "./pages/home/home.component";
+import { HomeComponent } from './pages/home/home.component';
 import { ParentDataComponent } from './component/parent-data/parent-data.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
-
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ParentDataComponent,
-        DirectivesComponent,
-        IfRenderComponent,
-        EventosComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HomeComponent
-    ]
+  declarations: [
+    AppComponent,
+    ParentDataComponent,
+    DirectivesComponent,
+    IfRenderComponent,
+    EventosComponent,
+    ChangeNumberComponent,
+    EmitterComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
